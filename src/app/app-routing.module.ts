5eb4e74c699eff0c785f5bd07114e3109e49f19f
sivaKumarv01 <sivakumarv2412@gmail.com>
2023-12-31 11:21:22 +0530

    'Upload'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Home/Home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"Home",
+    component:HomeComponent
+  },
+  {
+    path:"",
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/Header.component';
+import { HomeComponent } from './Home/Home.component';
+import { FooterComponent } from './Footer/Footer.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      HeaderComponent,
+      HomeComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
